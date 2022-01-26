beb37ef3d7f4c5cde0956d67321308756b9182de
eddymandran <eddy.mandran@gmail.com>
2022-01-26 15:55:30 +0100

    Working on the layout

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
-        <Text> the game screen !</Text>
+        <Text style={styles.title} > Start a New Game !</Text>
+        <View style={styles.inputContainer} >
+            <Text>Select a Number</Text>
+            <TextInput />
+            <View style={styles.buttonContainer} >
+                <Button title="Reset" onPress={() => console.log("button reset pressed")}/>
+                <Button title="Confirm" onPress={() => console.log("button confirm pressed")}/>
+
+            </View>
+        </View>
     </View>
   );
 };
@@ -14,6 +23,21 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems:'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: "80%",
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection:"row",
+        width: '100%',
+        justifyContent: 'space-around',
+        paddingHorizontal: 15
     }
    
 });
