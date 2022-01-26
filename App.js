ddbee3ea9f828ea45d37e68b801ecee156f07f1b
eddymandran <eddy.mandran@gmail.com>
2022-01-26 14:33:44 +0100

    Adding a screen component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import {StyleSheet, View} from 'react-native';
 import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
     return (
         <View style={styles.screen}>
             <Header title="Guess a Number" />
+            <StartGameScreen />
         </View>
     );
 }
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const StartGameScreen = props => {
+  return (
+    <View style={styles.screen}>
+        <Text> the game screen !</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems:'center'
+    }
+   
+});
+
+export default StartGameScreen;
