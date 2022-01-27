f966be71db2fd31f5ec99fbf52014b44febd610a
eddymandran <eddy.mandran@gmail.com>
2022-01-27 19:45:32 +0100

    A Synthetic Style "Cascade": Custom Wrapper Components & Global Styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
-import {Button, StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard, Alert} from "react-native";
+import {Alert, Button, Keyboard, StyleSheet, TouchableWithoutFeedback, View} from "react-native";
 import Card from "../components/Card";
 import Input from "../components/Input";
 import Colors from "../constants/colors"
 import NumberContainer from "../components/NumberContainer";
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+
 
 const StartGameScreen = props => {
 
@@ -41,7 +44,7 @@ const StartGameScreen = props => {
     if (confirmed) {
         confirmedOutput = (
             <Card style={styles.summaryContainer}>
-                <Text>You selected</Text>
+                <BodyText>You selected</BodyText>
                 <NumberContainer>{selectedNumber}</NumberContainer>
                 <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)}/>
             </Card>)
@@ -53,9 +56,9 @@ const StartGameScreen = props => {
             Keyboard.dismiss()
         }}>
             <View style={styles.screen}>
-                <Text style={styles.title}> Start a New Game !</Text>
+                <TitleText style={styles.title}> Start a New Game !</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false}
                            keyboardType="number-pad" maxLength={2} onChangeText={numberInputHandler}
                            value={enteredValue}/>
@@ -78,7 +81,8 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 10,
-        alignItems: 'center'
+        alignItems: 'center',
+        fontFamily: 'open-sans-bold'
     },
     title: {
         fontSize: 20,
