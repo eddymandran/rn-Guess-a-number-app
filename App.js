4008fe8377d6666843ffd640a8c399071bcf4243
eddymandran <eddy.mandran@gmail.com>
2022-01-27 18:15:34 +0100

    Adding Custom fonts

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import {useState} from "react";
 import {StyleSheet, View} from 'react-native';
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    })
+}
+
 export default function App() {
 
     const [userNumber, setUserNumber] = useState();
     const [guessRounds, setGuessRounds] = useState(0);
+    const [dataloaded, setDataloaded] = useState(false);
+
+    if (!dataloaded) {
+        return <AppLoading
+            startAsync={fetchFonts}
+            onFinish={() => setDataloaded(true)}
+            onError={(err) => console.log(err)}
+        />
+    }
 
     const configureNewGameHandler = () => {
-      setGuessRounds(0)
+        setGuessRounds(0)
         setUserNumber(null)
     }
 
@@ -23,24 +42,25 @@ export default function App() {
         setGuessRounds(numOfRounds)
     }
 
-    let content = <StartGameScreen onStartGame={startGameHandler} />
+    let content = <StartGameScreen onStartGame={startGameHandler}/>
 
     if (userNumber && guessRounds <= 0) {
-        content= <GameScreen userChoice={userNumber}  onGameOver={gameOverHandler}/>
+        content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
     } else if (guessRounds > 0) {
-        content= <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler}/>
+        content =
+            <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler}/>
     }
 
     return (
         <View style={styles.screen}>
-            <Header title="Guess a Number" />
+            <Header title="Guess a Number"/>
             {content}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    screen : {
+    screen: {
         flex: 1
     }
 });
